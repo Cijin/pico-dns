a6a77b2c933bcf23ea5b9c8510a01c5e4d7a85a1
sea gin <cijin@seagin.me>
2025-03-23 05:59:36 +0700

    move driver to it's own house

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const microzig = @import("microzig");
+const ssd1306 = @import("./ssd1306.zig");
 const rp2040 = microzig.hal;
 const time = microzig.drivers.time;
 const i2c = rp2040.i2c;
@@ -12,20 +13,12 @@ const pin_config = rp2040.pins.GlobalConfiguration{
         .direction = .out,
     },
 };
+
 const pins = pin_config.pins();
-
-const CONTROL_COMMAND = 0x00;
-const CONTROL_DATA = 0x40;
-const COMMAND_ADDRESSING_MODE = 0x20;
-const COMMAND_COLUMN_ADDRESS = 0x21;
-const COMMAND_PAGE_ADDRESS = 0x22;
-const init = [_]u8{ CONTROL_COMMAND, 0xAE, CONTROL_COMMAND, 0xA8, 0x1F, CONTROL_COMMAND, 0xD3, 0x00, CONTROL_COMMAND, 0x40, CONTROL_COMMAND, 0xA0, CONTROL_COMMAND, 0xC0, CONTROL_COMMAND, 0xDA, 0x02, CONTROL_COMMAND, 0x81, 0x7F, CONTROL_COMMAND, 0xA4, CONTROL_COMMAND, 0xD5, 0x80, CONTROL_COMMAND, 0x8D, 0x14, CONTROL_COMMAND, 0xAF };
-
 const i2c0 = i2c.instance.num(0);
 
 pub fn main() !void {
     pin_config.apply();
-
     const sda_pin = gpio.num(0);
     const scl_pin = gpio.num(1);
     inline for (&.{ scl_pin, sda_pin }) |pin| {
@@ -40,43 +33,14 @@ pub fn main() !void {
         pins.led.put(1);
     };
 
-    const a: i2c.Address = @enumFromInt(0x3C);
-
-    i2c0.write_blocking(a, &init, time.Duration.from_ms(500)) catch {
+    ssd1306.init(i2c0) catch {
         pins.led.put(1);
     };
 
-    const addressingMode = [3]u8{ CONTROL_COMMAND, COMMAND_ADDRESSING_MODE, 0x00 };
-    i2c0.write_blocking(a, &addressingMode, time.Duration.from_ms(500)) catch {
+    const buf: [512]u8 = .{0xFF} ** 512;
+    var r_idx: usize = 0;
+    _ = &r_idx;
+    ssd1306.write_data(i2c0, buf[r_idx..buf.len]) catch {
         pins.led.put(1);
     };
-
-    const columnAddress = [_]u8{
-        CONTROL_COMMAND,
-        COMMAND_COLUMN_ADDRESS,
-        0x00,
-        0x7F,
-        COMMAND_PAGE_ADDRESS,
-        0x00,
-        0x03,
-    };
-    i2c0.write_blocking(a, &columnAddress, time.Duration.from_ms(500)) catch {
-        pins.led.put(1);
-    };
-
-    var buf: [512]u8 = .{0x00} ** 512;
-    for (0..buf.len) |i| {
-        var data = [2]u8{ CONTROL_DATA, buf[i] };
-        i2c0.write_blocking(a, &data, time.Duration.from_ms(500)) catch {
-            pins.led.put(1);
-        };
-    }
-
-    buf = .{0xFF} ** 512;
-    for (0..buf.len) |i| {
-        var data = [2]u8{ CONTROL_DATA, buf[i] };
-        i2c0.write_blocking(a, &data, time.Duration.from_ms(500)) catch {
-            pins.led.put(1);
-        };
-    }
 }
---
+++ src/ssd1306.zig
@@ -0,0 +1,43 @@
+const microzig = @import("microzig");
+const rp2040 = microzig.hal;
+const time = microzig.drivers.time;
+const i2c = rp2040.i2c;
+
+const CONTROL_COMMAND = 0x00;
+const CONTROL_DATA = 0x40;
+const COMMAND_ADDRESSING_MODE = 0x20;
+const COMMAND_COLUMN_ADDRESS = 0x21;
+const COMMAND_PAGE_ADDRESS = 0x22;
+
+const INIT_SEQUENCE = [_]u8{ CONTROL_COMMAND, 0xAE, CONTROL_COMMAND, 0xA8, 0x1F, CONTROL_COMMAND, 0xD3, 0x00, CONTROL_COMMAND, 0x40, CONTROL_COMMAND, 0xA0, CONTROL_COMMAND, 0xC0, CONTROL_COMMAND, 0xDA, 0x02, CONTROL_COMMAND, 0x81, 0x7F, CONTROL_COMMAND, 0xA4, CONTROL_COMMAND, 0xD5, 0x80, CONTROL_COMMAND, 0x8D, 0x14, CONTROL_COMMAND, 0xAF };
+
+const a: i2c.Address = @enumFromInt(0x3C);
+
+pub fn init(pin: i2c.I2C) !void {
+    try pin.write_blocking(a, &INIT_SEQUENCE, time.Duration.from_ms(500));
+
+    const addressingMode = [3]u8{ CONTROL_COMMAND, COMMAND_ADDRESSING_MODE, 0x00 };
+    try pin.write_blocking(a, &addressingMode, time.Duration.from_ms(500));
+
+    const column_page_address = [_]u8{
+        CONTROL_COMMAND,
+        COMMAND_COLUMN_ADDRESS,
+        0x00,
+        0x7F,
+        COMMAND_PAGE_ADDRESS,
+        0x00,
+        0x03,
+    };
+    try pin.write_blocking(a, &column_page_address, time.Duration.from_ms(500));
+
+    const buf: [512]u8 = .{0x00} ** 512;
+    for (0..buf.len) |i| {
+        try pin.write_blocking(a, &[2]u8{ CONTROL_DATA, buf[i] }, time.Duration.from_ms(500));
+    }
+}
+
+pub fn write_data(pin: i2c.I2C, data: []const u8) !void {
+    for (0..data.len) |i| {
+        try pin.write_blocking(a, &[2]u8{ CONTROL_DATA, data[i] }, time.Duration.from_ms(500));
+    }
+}
